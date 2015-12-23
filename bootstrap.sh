6dfa53ab1682cf17e196cf1d5123b5a668ad3c66
Jolan <spam00fr@yahoo.fr>
2015-12-23 14:33:04 +0100

    added a simple bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+dotfiles=`dirname $0`;
+
+if [[ ! -f ${HOME}/.zgen/zgen.zsh ]];then
+	echo 'installing zgen';
+	git clone https://github.com/tarjoilija/zgen.git ${HOME}/.zgen
+fi
+
+for file in zshrc vimrc ;do
+	path=$HOME/.$file
+	if [[ ! -h $path ]];then
+		if [[ -f $path ]];then
+			echo "saving previous .$file to $path.backup"
+			mv $path $path.backup
+		fi
+		echo "creating a link to $file"
+		ln -s $dotfiles/$file $path
+	fi
+done
